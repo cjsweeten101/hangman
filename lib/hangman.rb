75feb4ed82cb5af92bf8e12916f21fecd2c07e21
Clayton Sweeten <cjsweeten101@gmail.com>
2025-01-25 12:09:28 -0700

    draw hangman

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,30 +2,15 @@
 
 # Implements the hangman game object
 class Hangman
-  @board_states = ["O'\n'", '/', '|', "\\'\n'", '/', '\\']
   @correct_guesses = []
   @incorrect_guesses = []
 
   def initialize(secret_word)
+    @board_states = ["   O\n", '  /', '|', "\\\n", '  / ', '\\']
     @secret_word_array = secret_word.split('')
-    @guess_count = 0
-    p @secret_word_array.join('')
+    @incorrect_guess_count = 0
     initialize_board
     draw_board
-  end
-
-  def make_guess(letter)
-    if @secret_word_array.include? letter
-      @secret_word_array.each_with_index do |l, i|
-        @correct_guesses[i] = letter if letter == l
-      end
-    else
-      @guess_count += 1
-    end
-  end
-
-  def draw_board
-    p @correct_guesses.join('')
   end
 
   def initialize_board
@@ -34,6 +19,30 @@ class Hangman
       result << '-'
     end
     @correct_guesses = result
+  end
+
+  def make_guess(letter)
+    if @secret_word_array.include? letter
+      @secret_word_array.each_with_index do |l, i|
+        @correct_guesses[i] = letter if letter == l
+      end
+    else
+      @incorrect_guess_count += 1
+    end
+  end
+
+  def draw_board
+    puts @secret_word_array.join('')
+    puts create_hangman_drawing
+    puts @correct_guesses.join('')
+  end
+
+  def create_hangman_drawing
+    result = "---|\n"
+    @incorrect_guess_count.times do |i|
+      result += @board_states[i]
+    end
+    result
   end
 
   def game_over?
